6e6916fed457c0f59ecd5c2360b875ac2b188bfe
hyoon <hyoong2006@gmail.com>
2025-05-26 11:46:59 +0800

    login update 2

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
     <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
     <script src="https://kit.fontawesome.com/b1e4b09c02.js" crossorigin="anonymous"></script>
     <title>Signup</title>
 </head>
